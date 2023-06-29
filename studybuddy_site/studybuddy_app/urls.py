fab237c4f82dcf9dae1156659bff6c9c1b180c77
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-06-29 18:46:38 +0200

    Part 3: simple index

--- studybuddy_site/studybuddy_app/urls.py
+++ studybuddy_site/studybuddy_app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    # ex: /polls/5/
+    path("<int:meetup_id>/", views.detail, name="detail"),
+    # ex: /polls/5/rsvp/
+    path("<int:meetup_id>/rsvp/", views.rsvp, name="rsvp"),
 ]
+
--- studybuddy_site/studybuddy_app/views.py
+++ studybuddy_site/studybuddy_app/views.py
@@ -2,6 +2,20 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Meetup
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the studybuddy index.")
+    next_meetups_list = Meetup.objects.order_by("start_time")[:5]
+    output = "Hello, world. You're at the studybuddy index."
+    output += ", ".join([m.title for m in next_meetups_list])
+    return HttpResponse(output)
+
+
+def detail(request, meetup_id):
+    return HttpResponse("You're looking at meeting %s." % meetup_id)
+
+
+def rsvp(request, meetup_id):
+    return HttpResponse("You rsvp on meetup %s." % meetup_id)
+
