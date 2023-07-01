e6301638bb8ebcc3f7a5abbc6c4adc3a402ab6b1
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-07-01 20:59:38 +0200

    Part 7: Customize Admin App

--- studybuddy_site/studybuddy_app/admin.py
+++ studybuddy_site/studybuddy_app/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 from .models import Meetup
 
-admin.site.register(Meetup)
+
+# admin.site.register(Meetup)
+
+class MeetupAdmin(admin.ModelAdmin):
+    # fields = ["location", "title", "start_time", "end_time"]
+    
+    fieldsets = [
+        (None, {"fields": ["location", "title"]}),
+        ("Date information", {"fields": ["start_time", "end_time"]}),
+    ]
+    list_display = ["title", "start_time"]
+
+
+admin.site.register(Meetup, MeetupAdmin)
